f147e6e622e4ed9a373fe31536a9a0d028f79bba
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-05-14 10:55:32 +0000

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |i|
+    Book.create(
+        title: "test#{i+1}",
+        body: "test#{i+1}"
+    )
+end
